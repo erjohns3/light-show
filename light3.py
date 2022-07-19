2691285c7fe30f2e6b354809be930fdbaad8000f
Andrew Duerig <gamechefproductions@gmail.com>
2022-07-18 20:08:44 -0400

    Idk

--- light3.py
+++ light3.py
@@ -1,3 +1,5 @@
+from helpers import *
+
 import threading
 import time
 import sys
@@ -11,72 +13,60 @@ import argparse
 import os
 from os import path
 import math
-import pigpio
-import board
-import busio
-import adafruit_pca9685
 
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
+try:
+    SUB_BEATS = 24
+    LIGHT_COUNT = 7
 
-pca.frequency = 200
-
-SUB_BEATS = 24
-LIGHT_COUNT = 7
-
-args = False
-pi = None
-curr_modes = []
-curr_bpm = 120
-tick_start = time.perf_counter()
-
-light_lock = threading.Lock()
-light_task = False
-
-async def set_light(new_modes, new_bpm):
-    global curr_modes
-    global curr_bpm
-    global tick_start
-
-    light_lock.acquire()
-    curr_modes = new_modes
-    curr_bpm = new_bpm
+    args = False
+    pi = None
+    curr_modes = []
+    curr_bpm = 120
     tick_start = time.perf_counter()
-    light_lock.release()
-    print("mode: " + str(curr_modes))
 
-####################################
+    light_lock = threading.Lock()
+    light_task = False
 
-async def light():    
 
-    while True:
-        light_lock.acquire()
-        modes = curr_modes
-        rate = curr_bpm / 60 * SUB_BEATS
-        time_start = tick_start
-        light_lock.release()
+    import pigpio
+    import board
+    import busio
+    import adafruit_pca9685
+
+    i2c = busio.I2C(board.SCL, board.SDA)
+    pca = adafruit_pca9685.PCA9685(i2c)
+    
+    pca.frequency = 200
+
+except Exception as e:
+    print(f'{bcolors.FAIL}Could not import / setup hardware due to exception "{e}"{bcolors.ENDC}')
+
+
+
+################################################
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
         
-        time_curr = time.perf_counter()
-        time_diff = time_curr - time_start
-        num = int(time_diff * rate)
-        time_delay = ((num + 1) / rate) - time_diff
-        
-        for i in range(LIGHT_COUNT):
-            level = 0
-            for mode in modes:
-                index = num % len(light_array[mode])
-                level += light_array[mode][index][i]
-            pca.channels[i].duty_cycle = max(0, min(0xFFFF, int(level * 0xFFFF / 100)))
-      
-        await asyncio.sleep(time_delay)
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
 
-#################################################
+def run_http_server():
+    httpd = http.server.ThreadingHTTPServer(("", PORT), Handler)
+    print("serving at port " + str(PORT), flush=True)
+    httpd.serve_forever()
+
+################################################
 
 async def init(websocket, path):
     global light_task
 
     if not light_task:
-        light_task = asyncio.create_task(light())
+        light_task = asyncio.create_task(light(light_array))
 
     message = {
         'config': config,
@@ -105,17 +95,17 @@ async def init(websocket, path):
 
             if msg['type'] == 'apply':
                 if msg['modes'] != '' and msg['rate'] != '':
-                    mode = msg['modes']
+                    modes = msg['modes']
                     bpm = float(msg['rate'])
                     if bpm > 0:
-                        await set_light(mode, bpm)
+                        await set_light(modes, bpm)
 
             elif msg['type'] == 'preview':
                 if msg['modes'] != '' and msg['rate'] != '':
-                    mode = msg['modes']
+                    modes = msg['modes']
                     bpm = float(msg['rate'])
                     if bpm > 0:
-                        await set_light(mode, bpm)
+                        await set_light(modes, bpm)
 
             message = {
                 'status': {
@@ -129,7 +119,9 @@ async def init(websocket, path):
             except:
                 print("socket send failed", flush=True)
 
-######################################
+#################################################
+
+
 
 def setup_pigpio():
     global pi
@@ -142,70 +134,99 @@ def setup_pigpio():
 
 #################################################
 
-class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
-        self.send_header("Pragma", "no-cache")
-        self.send_header("Expires", "0")
-        http.server.SimpleHTTPRequestHandler.end_headers(self)
+async def set_light(new_modes, new_bpm, debug=True):
+    global curr_modes
+    global curr_bpm
+    global tick_start
+
+    light_lock.acquire()
+    curr_modes = new_modes
+    curr_bpm = new_bpm
+    # tick_start = time.perf_counter()
+    light_lock.release()
+    if debug:
+        print("mode: " + str(curr_modes))
+
+####################################
+
+async def light(light_array, optional_callback=None):
+    while True:
+        light_lock.acquire()
+        modes = curr_modes
+        rate = curr_bpm / 60 * SUB_BEATS
+        time_start = tick_start
+        light_lock.release()
         
-PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
+        time_curr = time.perf_counter()
+        time_diff = time_curr - time_start
+        num = int(time_diff * rate)
+        time_delay = ((num + 1) / rate) - time_diff
+        
+        all_levels = []
+        for i in range(LIGHT_COUNT):
+            level = 0
+            for mode in modes:
+                index = num % len(light_array[mode])
+                level += light_array[mode][index][i]
+            if optional_callback:
+                all_levels.append(level)
+            else:
+                pca.channels[i].duty_cycle = max(0, min(0xFFFF, int(level * 0xFFFF / 100)))
+        if optional_callback:
+            await optional_callback(all_levels)
 
-def http_server(testing=False):
-    httpd = http.server.ThreadingHTTPServer(("", PORT), Handler)
-    print("serving at port " + str(PORT), flush=True)
-    httpd.serve_forever()
-
-################################################
-
-loc = pathlib.Path(__file__).parent.absolute()
-drink_io_folder = str(loc)
-
-with open(path.join(drink_io_folder, 'config.json'), 'r') as f:
-    config = json.loads(f.read())
-
-light_dict = {}
-light_array = {}
-
-for mode in config:
-    light_dict[mode] = {}
-    config[mode]['beats'][str(config[mode]['length']+1)] = False
-    beats = list(config[mode]['beats'].keys())
-    config[mode]['beats'][str(config[mode]['length']+1)] = config[mode]['beats'][beats[0]]
-    for i in range(len(beats)):
-        curr_beat = round((eval(beats[i])-1)*SUB_BEATS) / SUB_BEATS
-        light_dict[mode][curr_beat] = config[mode]['beats'][beats[i]]
-        if 'interpolation' in config[mode] and config[mode]['interpolation'] == 'hold' and i+1 < len(beats):
-            next_beat = eval(beats[i+1]) - 1 - (1/SUB_BEATS)
-            if abs(next_beat - curr_beat) > 0.5/SUB_BEATS:
-                light_dict[mode][next_beat] = light_dict[mode][curr_beat]
-
-for mode in config:
-    print(f'{mode}')
-    for beat in light_dict[mode]:
-        print(f'    {beat}: {round(light_dict[mode][beat][0])}, {round(light_dict[mode][beat][1])}, {round(light_dict[mode][beat][2])}')
+        await asyncio.sleep(time_delay)
 
 
-for mode in config:
-    light_array[mode] = [False] * round(config[mode]['length'] * SUB_BEATS)
-    beats = list(light_dict[mode].keys())
-    for i in range(len(beats)-1):
-        start_index = round(beats[i] * SUB_BEATS)
-        end_index = round(beats[i+1] * SUB_BEATS)
-        start_light = light_dict[mode][beats[i]]
-        end_light = light_dict[mode][beats[i+1]]
 
-        for j in range(start_index, end_index):
-            light_array[mode][j] = [0] * LIGHT_COUNT
-            shift = (j - start_index) / (end_index - start_index)
-            for k in range(LIGHT_COUNT):
-                light_array[mode][j][k] = (end_light[k] * shift) + (start_light[k] * (1 - shift))
+######################################
 
-for mode in config:
-    print(f'{mode}')
-    for i in range(len(light_array[mode])):
-        print(f'    {i}: {round(light_array[mode][i][0])}, {round(light_array[mode][i][1])}, {round(light_array[mode][i][2])}')
+def read_config(config_filepath):    
+    with open(config_filepath, 'r') as f:
+        config = json.loads(f.read())
+
+    light_dict = {}
+    light_array = {}
+
+    for mode in config:
+        light_dict[mode] = {}
+        config[mode]['beats'][str(config[mode]['length']+1)] = False
+        beats = list(config[mode]['beats'].keys())
+        config[mode]['beats'][str(config[mode]['length']+1)] = config[mode]['beats'][beats[0]]
+        for i in range(len(beats)):
+            curr_beat = round((eval(beats[i])-1)*SUB_BEATS) / SUB_BEATS
+            light_dict[mode][curr_beat] = config[mode]['beats'][beats[i]]
+            if 'interpolation' in config[mode] and config[mode]['interpolation'] == 'hold' and i+1 < len(beats):
+                next_beat = eval(beats[i+1]) - 1 - (1/SUB_BEATS)
+                if abs(next_beat - curr_beat) > 0.5/SUB_BEATS:
+                    light_dict[mode][next_beat] = light_dict[mode][curr_beat]
+
+    for mode in config:
+        print(f'{mode}')
+        for beat in light_dict[mode]:
+            print(f'    {beat}: {round(light_dict[mode][beat][0])}, {round(light_dict[mode][beat][1])}, {round(light_dict[mode][beat][2])}')
+
+
+    for mode in config:
+        light_array[mode] = [False] * round(config[mode]['length'] * SUB_BEATS)
+        beats = list(light_dict[mode].keys())
+        for i in range(len(beats)-1):
+            start_index = round(beats[i] * SUB_BEATS)
+            end_index = round(beats[i+1] * SUB_BEATS)
+            start_light = light_dict[mode][beats[i]]
+            end_light = light_dict[mode][beats[i+1]]
+
+            for j in range(start_index, end_index):
+                light_array[mode][j] = [0] * LIGHT_COUNT
+                shift = (j - start_index) / (end_index - start_index)
+                for k in range(LIGHT_COUNT):
+                    light_array[mode][j][k] = (end_light[k] * shift) + (start_light[k] * (1 - shift))
+
+    for mode in config:
+        print(f'{mode}')
+        for i in range(len(light_array[mode])):
+            print(f'    {i}: {round(light_array[mode][i][0])}, {round(light_array[mode][i][1])}, {round(light_array[mode][i][2])}')
+    return light_array
 
 ##################################################
 
@@ -221,25 +242,21 @@ def signal_handler(sig, frame):
 
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+
 
 #################################################
 
 
-def main():
-    global args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--testing', action='store_true', default=False,
-                   help='To run webserver without drinkmaker attached')
-    parser.add_argument('--local', action='store_true', default=False,
-                   help='To run webserver without drinkmaker attached')
-    args = parser.parse_args()
 
-    if not args.testing:
-        setup_pigpio()
+if __name__ == "__main__":
+    setup_pigpio()
 
-    http_thread = threading.Thread(target=http_server, args=[args.testing], daemon=True)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    light_array = read_config(python_file_directory.joinpath('config.json'))
+
+    http_thread = threading.Thread(target=run_http_server, args=[], daemon=True)
     http_thread.start()
 
     loop = asyncio.new_event_loop()
@@ -248,7 +265,3 @@ def main():
     start_server = websockets.serve(init, "0.0.0.0", 8765)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
-
-
-if __name__ == "__main__":
-    main()
---
+++ sound_helpers.py
@@ -0,0 +1,158 @@
+# example call
+# python play_some_sound_ffmpeg.py data\f538d56c8aa48cea62c35aa01438ff92cde5708ec59c69e9e1be65ff.mp3 42 8
+
+import os
+import subprocess
+import threading
+import multiprocessing
+import pathlib
+
+from helpers import *
+
+audio_lock = threading.Lock()
+audio_procs = []
+
+
+
+
+
+def play_audio_async(filepath, volume=100, paused=False):
+    global is_paused
+    is_paused = True
+
+    proc = multiprocessing.Process(target=play_sound_with_mpv, args=[filepath, volume, paused])
+    with audio_lock:
+        audio_procs.append(proc)
+    proc.start()
+
+def is_audio_running():
+    return any(map(lambda x: x.is_alive(), audio_procs))
+
+def play_audio_seek_normalize_async(filepath, seconds_seek_to, seconds_to_play, normalize_peak_volume_decibals=False):
+    proc = multiprocessing.Process(target=play_audio_seek_normalize_blocking, args=(filepath, seconds_seek_to, seconds_to_play, normalize_peak_volume_decibals))
+    with audio_lock:
+        audio_procs.append(proc)
+    proc.start()
+
+
+def play_audio_seek_normalize_blocking(filepath, seconds_seek_to, seconds_to_play, normalize_peak_volume_decibals=False):
+    output_directory = pathlib.Path(__file__).parent.resolve().joinpath('data').joinpath('final_audio_clips')
+    altered_mp3_filepath = create_then_normalize_mp3(filepath, output_directory, seconds_seek_to, seconds_to_play)
+    play_sound_with_ffplay(altered_mp3_filepath)
+
+
+def stop_audio():
+    with audio_lock:
+        while audio_procs:
+            proc = audio_procs[-1]
+            print(proc.is_alive())
+            proc.terminate()
+            proc.join()
+            audio_procs.pop()
+
+# example call: ffplay -i data\f538d56c8aa48cea62c35aa01438ff92cde5708ec59c69e9e1be65ff.mp3 -autoexit -nodisp -af 'dynaudnorm=n=0:c=1'
+def play_sound_with_ffplay(audio_path, normalize_peak_volume_decibals=False, volume=100):
+    if is_linux_root():
+        print(f'{bcolors.FAIL}WARNING: you are running as root, this probably will cause problems with audio{bcolors.ENDC}')
+
+    extra_args = []
+    if normalize_peak_volume_decibals:
+        extra_args += ['-af', 'loudnorm=I=-16:LRA=11:TP=-1.5']
+        # ffplay_args.append("-af 'dynaudnorm=n=0:c=1'")
+
+    run_command_blocking([
+        'ffplay',
+        '-autoexit',
+        '-nodisp',
+        '-volume', 
+        volume,
+        str(audio_path)
+    ] + extra_args, debug=True, print_std_out=True)
+
+
+
+# i think full list of cmds
+# https://mpv.io/manual/master/#json-ipc-client-name
+# https://mpv.io/manual/master/#properties
+mpv_pause_cmd = '''\
+echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpvsocket
+'''
+
+mpv_unpause_cmd = '''\
+echo '{ "command": ["set_property", "pause", false] }' | socat - /tmp/mpvsocket
+'''
+
+
+global is_paused
+is_paused = False
+
+def kill_mpv():
+    os.system('killall mpv')
+
+def toggle_pause_async_mpv():
+    global is_paused
+    if is_paused:
+        os.system(mpv_unpause_cmd)
+    else:
+        os.system(mpv_pause_cmd)
+    is_paused = not is_paused
+
+
+def play_sound_with_mpv(audio_path, volume=100, paused=False):
+    if is_linux_root():
+        print(f'{bcolors.FAIL}WARNING: you are running as root, this probably will cause problems with audio{bcolors.ENDC}')
+    extra_args = []
+    if paused:
+        extra_args += ['--pause']
+    run_command_blocking([
+        'mpv',
+        '--no-resume-playback',
+        f'--volume={volume}',
+        '--input-ipc-server=/tmp/mpvsocket',
+        'no-osc',
+        'use-text-osd=no',
+        'osd-level=1',
+        'term-osd=force',
+        '--no-video',
+        str(audio_path)
+    ] + extra_args, debug=True, print_std_out=True)
+
+
+
+
+
+def create_then_normalize_mp3(input_filepath, output_directory, seconds_seek_to, seconds_to_play):
+    not_normalized_mp3_filepath = create_mp3(input_filepath, output_directory, seconds_seek_to, seconds_to_play)
+    return normalize_mp3(not_normalized_mp3_filepath, output_directory)
+
+def normalize_mp3(input_filepath, output_directory):
+    output_filepath = os.path.join(output_directory, f'{random_letters(10)}.mp3')
+    ffmpeg_args = [
+        '-i', 
+        input_filepath,
+        '-af', 
+        'loudnorm=I=-16:LRA=11:TP=-1.5',
+        output_filepath
+    ]
+    run_command_blocking(['ffmpeg'] + ffmpeg_args)
+    return output_filepath
+
+
+def create_mp3(input_filepath, output_directory, seconds_seek_to, seconds_to_play):
+    output_filepath = os.path.join(output_directory, f'{random_letters(10)}.mp3')
+    ffmpeg_args = [
+        '-i', 
+        input_filepath, 
+        '-ss', 
+        seconds_to_hmsm_string(seconds_seek_to), 
+        '-to', 
+        seconds_to_hmsm_string(seconds_seek_to + seconds_to_play), 
+        output_filepath
+    ]
+    run_command_blocking(['ffmpeg'] + ffmpeg_args)
+    return output_filepath
+
+def get_audio_clip_length(filename):
+    ffprobe_args = ['-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', filename]
+    return float(run_command_blocking(['ffprobe'] + ffprobe_args))
+

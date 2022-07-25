4749f37872d213e624c64462be47a90a6a1df18f
Andrew Duerig <gamechefproductions@gmail.com>
2022-07-25 11:16:56 -0400

    Yeah

---
+++ README.md
@@ -0,0 +1,20 @@
+# club time
+
+## ideas for python audio feature extraction:
+* https://github.com/Yaafe/Yaafe
+* https://github.com/tyiannak/pyAudioAnalysis
+    * doc is bit sparse, really just this: https://github.com/tyiannak/pyAudioAnalysis/wiki/2.-General
+* https://github.com/librosa/librosa
+* https://github.com/aubio/aubio/tree/master/python/demos
+    * c library with python module
+
+
+## random links
+* identifying strongest beat
+    * https://stackoverflow.com/questions/54718744/python-audio-analysis-find-real-time-values-of-the-strongest-beat-in-each-meter
+
+* tap beat for bpm
+    * https://www.all8.com/tools/bpm.htm
+
+* fixing octave error (double or half bpm instead of dead on)
+    * https://stackoverflow.com/questions/61621282/how-can-we-improve-tempo-detection-accuracy-in-librosa
---
+++ generate_show.py
@@ -0,0 +1,102 @@
+import pathlib
+import json
+import re
+
+import sys
+from essentia import *
+from essentia.standard import *
+from pydub import AudioSegment
+# from pylab import *
+
+from helpers import * 
+
+
+song_name = 'musician.mp3'
+song_filepath = python_file_directory.joinpath('data').joinpath(song_name).resolve().relative_to(python_file_directory)
+
+
+
+audio = MonoLoader(filename=str(song_filepath))()
+
+rhythm_extractor = RhythmExtractor2013(method="multifeature")
+bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(audio)
+
+rounded_bpm = int(round(bpm, 0))
+print(f'BPM: real: {bpm}, rounded: {rounded_bpm}',)
+print(f'Beat estimation confidence: {beats_confidence}')
+
+# Write to an audio file in a temporary directory.
+beats_filepath = pathlib.Path('data').joinpath('nonsense').joinpath('temp_beats_output.dat')
+with open(beats_filepath, 'w') as f:
+    f.writelines([' '.join(map(str, beats))])
+
+
+# Mark beat positions in the audio and write it to a file.
+# Use beeps instead of white noise to mark them, as it is more distinctive.
+
+
+marker = AudioOnsetsMarker(onsets=beats, type='beep')
+marked_audio = marker(audio)
+click_filepath = pathlib.Path('data').joinpath('nonsense').joinpath('click_file_for_essentia.mp3')
+MonoWriter(filename=str(click_filepath))(marked_audio)
+
+
+new_show = {
+    'song_name': str(song_filepath),
+    'bpm': rounded_bpm,
+    'show_timing_type': 'seconds',
+    # 'delay': 0.0,
+    'show': [],
+}
+
+
+# making inteligent decicions or something
+sound = AudioSegment.from_file(song_filepath, format='mp3')
+
+
+
+modes_to_cycle = ['Red Top', 'Green Top']
+for index, second in enumerate(beats):
+    mode = modes_to_cycle[index % len(modes_to_cycle)]
+    new_show['show'].append([mode, float(second)])
+    
+
+# print(new_show)
+
+
+
+
+# writing into a show
+with open('shows.json') as f:
+    shows_json = json.loads(f.read())
+
+
+
+shows_json[os.path.splitext(song_name)[0]] = new_show
+
+
+
+
+
+def eliminate(string, matches):
+    found = set()
+    for match in matches:
+        if match in found:
+            continue
+        found.add(match)
+        string = string.replace(match, '')
+    return string
+
+with open('shows.json', 'w') as f:
+    shows_json_str = json.dumps(shows_json, indent=4, sort_keys=True)
+
+    before_string_matches = re.findall(r"\[(\s+)\"", shows_json_str)
+    shows_json_str = eliminate(shows_json_str, before_string_matches)
+
+    after_digit = re.findall(r"\d(\s+)\]", shows_json_str)
+    shows_json_str = eliminate(shows_json_str, after_digit)
+    shows_json_str = shows_json_str.replace('[[', '[\n            [')
+    shows_json_str = shows_json_str.replace('],[', '],\n            [')
+    
+    f.writelines([shows_json_str])
+

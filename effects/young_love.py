f3eb1ca46867d8199c9f6089b4157693a2038579
Andrew Paul Duerig <duerig@fb.com>
2022-08-19 21:46:39 -0400

    sadasd

---
+++ effects/young_love.py
@@ -0,0 +1,19 @@
+effects = {
+    "love kicks": {
+        "length": 1,
+        "beats": [
+            [1, "Green bottom", 0.25, 1, 0],
+        ],
+    },
+    "young_love show": {
+        "beats": [
+            [1, "rainbow good slow top", 32],
+            [33, "love kicks", 128],
+        ],
+        "delay_lights": .08,
+        "skip_song": 0,
+        "bpm": 138,
+        "song_path": "songs/Superhumanoids - Too Young For Love.ogg",
+        "profiles": ["Shows"],
+    },
+}

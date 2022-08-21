38c2ffed3163eb7ae6bc26a1b24b5fd71fae0f23
Andrew Duerig <gamechefproductions@gmail.com>
2022-08-21 05:13:37 -0400

    yeah

--- effects/basics.py
+++ effects/basics.py
@@ -1,31 +1,31 @@
 effects = {
     # pulses and strobes
-    "Strobe":{
+    "Strobe": {
         "length": 0.2,
         "beats": [
             [1, [100, 100, 100, 0, 0, 0, 0], 0.07],
         ],
     },
-    "UV Strobe":{
+    "UV Strobe": {
         "length": 0.2,
         "beats": [
             [1, [0, 0, 0, 0, 0, 0, 100], 0.07],
         ],
     },
-    "Pulse":{
+    "Pulse": {
         "length": 1,
         "beats": [
             [1, [100, 100, 100, 0, 0, 0, 0], 0.9, 1, 0],
         ],
     },
-    "UV pulse":{
+    "UV pulse": {
         "length": 1,
         "loop": False,
         "beats": [
             [1, [0, 0, 0, 0, 0, 0, 100], 1, 1, 0, 0],
         ]
     },
-    "UV pulse slow":{
+    "UV pulse slow": {
         "length": 2,
         "loop": False,
         "beats": [
@@ -34,48 +34,57 @@ effects = {
     },
 
     # Sidechain stuff
-    "Nothing":{
+    "Nothing": {
         "length": 1,
         "beats": [
             [1, [-1000, -1000, -1000, -1000, -1000, -1000, -1000], 1],
         ],
     },
-    "Sidechain top r":{
+    "Sidechain top r": {
         "length": 1,
         "beats": [
             [1, [-100, 0, 0, 0, 0, 0, 0], 1],
         ],
     },
-    "Sidechain top g":{
+    "Sidechain top g": {
         "length": 1,
         "beats": [
             [1, [0, -100, 0, 0, 0, 0, 0], 1],
         ],
     },
-    "Sidechain top b":{
+    "Sidechain top b": {
         "length": 1,
         "beats": [
             [1, [0, 0, -100, 0, 0, 0, 0], 1],
         ],
     },
-    "Sidechain top bg":{
+    "Sidechain top bg": {
         "length": 1,
         "beats": [
             [1, [0, -100, -100, 0, 0, 0, 0], 1],
         ],
     },
-    "Sidechain top rbg":{
+    "Sidechain top rbg": {
         "length": 1,
         "beats": [
             [1, [-100, -100, -100, 0, 0, 0, 0], 1],
         ],
     },
-    "Sidechain bottom g":{
+    "Sidechain bottom g": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 0, -100, 0, 0], 1],
         ],
     },
+
+    # flashes
+    "White flash": {
+        "length": 1,
+        "beats": {
+            "1": [[100, 100, 100, 100, 100, 100, 0], 0.3, 0.1, 0]
+        }
+    },
+
 
     # timings
     "RBBB 1 bar": {
@@ -89,140 +98,140 @@ effects = {
     },
 
     ##################################  colors
-    "UV":{
+    "UV": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 0, 0, 0, 100], 1],
         ],
     },
-    "White top":{
+    "White top": {
         "length": 1,
         "beats": [
             [1, [100, 100, 100, 0, 0, 0, 0], 1],
         ],
     },
-    "White bottom":{
+    "White bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 100, 100, 100, 0], 1],
         ],
     },
-    "Red top":{
+    "Red top": {
         "length": 1,
         "beats": [
             [1, [100, 0, 0, 0, 0, 0, 0], 1],
         ],
         "trigger": "hold"
     },
-    "Red bottom":{
+    "Red bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 100, 0, 0, 0], 1],
         ],
     },
-    "Green top":{
+    "Green top": {
         "length": 1,
         "beats": [
             [1, [0, 100, 0, 0, 0, 0, 0], 1],
         ],
     },
-    "Green bottom":{
+    "Green bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 0, 100, 0, 0], 1],
         ],
     },
-    "Blue top":{
+    "Blue top": {
         "length": 1,
         "beats": [
             [1, [0, 0, 100, 0, 0, 0, 0], 1],
         ],
     },
-    "Blue bottom":{
+    "Blue bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 0, 0, 100, 0], 1],
         ],
     },
-    "Orange top":{
+    "Orange top": {
         "length": 1,
         "beats": [
             [1, [100, 40, 10, 0, 0, 0, 0], 1],
         ],
     },
-    "Orange bottom":{
+    "Orange bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 100, 40, 10, 0], 1],
         ],
     },
-    "Firebrick top":{
+    "Firebrick top": {
         "length": 1,
         "beats": [
             [1, [178, 34, 34, 0, 0, 0, 0], 1],
         ],
     },
-    "Firebrick bottom":{
+    "Firebrick bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 178, 34, 34, 0], 1],
         ],
     },
-    "Cyan top":{
+    "Cyan top": {
         "length": 1,
         "beats": [
             [1, [10, 50, 100, 0, 0, 0, 0], 1],
         ],
     },
-    "Cyan bottom":{
+    "Cyan bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 10, 50, 100, 0], 1],
         ],
     },
-    "Yellow top":{
+    "Yellow top": {
         "length": 1,
         "beats": [
             [1, [80, 100, 10, 0, 0, 0, 0], 1],
         ],
     },
-    "Yellow bottom":{
+    "Yellow bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 80, 100, 10, 0], 1],
         ],
     },
-    "Pink top":{
+    "Pink top": {
         "length": 1,
         "beats": [
             [1, [100, 10, 30, 0, 0, 0, 0], 1],
         ],
     },
-    "Pink bottom":{
+    "Pink bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 100, 10, 30, 0], 1],
         ],
     },
-    "Indigo top":{
+    "Indigo top": {
         "length": 1,
         "beats": [
             [1, [25, 0, 70, 0, 0, 0, 0], 1],
         ],
     },
-    "Indigo bottom":{
+    "Indigo bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 40, 0, 90, 0], 1],
         ],
     },
-    "Purple bottom":{
+    "Purple bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 100, 0, 100, 0], 1],
         ],
     },
-    "Rosy brown bottom":{
+    "Rosy brown bottom": {
         "length": 1,
         "beats": [
             [1, [0, 0, 0, 70, 30, 30, 0], 1],
---
+++ effects/cnam.py
@@ -0,0 +1,44 @@
+effects = {
+    "cnam top colors": {
+        "length": 2,
+        "beats": [
+            [1, "Green top", 1, 0.2, 0.2],
+            [1.5, "Cyan top", 1, 0.2, 0.2],
+            [2, "Blue top", 1, 0.2, 0.2],
+            [2.5, "Red top", 1, 0.2, 0.2],            
+        ],
+    },
+    "cnam bass hits": {
+        "length": 8,
+        "beats": [
+            [1, "Firebrick bottom", .8, 1, 0.1], 
+            [1, "Sidechain top rbg", .8, 1, 0.1],
+        ],
+    },
+    "cnam faster bass hits": {
+        "length": 2,
+        "beats": [
+            [1, "Firebrick bottom", .8, 1, 0.1], 
+            [1, "Sidechain top rbg", .8, 1, 0.1],
+        ],
+    },
+    "cnam drop": {
+        "beats": [
+            [1, "cnam top colors", 96], 
+            [1, "cnam bass hits", 32],
+            [1, "cnam faster bass hits", 64],
+        ],
+    },
+    "cnam show": {
+        "beats": [
+            [1, "wandering", 64, 0, 1],
+            [65, "wandering", 128],
+            [193, "cnam drop", 96],
+        ],
+        "delay_lights": .22,
+        "skip_song": 0,
+        "bpm": 120,
+        "song_path": "songs/Porter Robinson x Illenium x Said the Sky Mix by C-Nam.ogg",
+        "profiles": ["Shows"],
+    },
+}

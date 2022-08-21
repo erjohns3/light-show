3bc365991d1f2329b20d0b94f7f4f12ab5ce5417
Andrew Duerig <gamechefproductions@gmail.com>
2022-08-21 05:02:18 -0400

    Yeah

--- effects/okaerinasai.py
+++ effects/okaerinasai.py
@@ -4,16 +4,16 @@ effects = {
     # 129 - 200: verse 1 natsu no (lyrics, wubs)
     # 200 - 264: verse 2
     # 265 - 393
-    "okaerinasai show":{
-        "beats":{
-            "1": [["RBBB 1 bar", 64]],
-            "65": [["wandering", 64]],
-            "129": [["RBBB 1 bar", 64]],
-            "193": [["Pink bottom", 8]],
-            "201": [["Ghosts bassline", 64]],
-            "265": [["Ghosts UV", 128]],
-            "394": [["Blue top", 64]],
-        },
+    "okaerinasai show": {
+        "beats": [
+            [1, "RBBB 1 bar", 64],
+            [65, "wandering", 64],
+            [129, "RBBB 1 bar", 64],
+            [193, "Pink bottom", 8],
+            [201, "Ghosts bassline", 64],
+            [265, "Ghosts UV", 128],
+            [394, "Blue top", 64],
+        ],
         "delay_lights": .65,
         "skip_song": 0,
         "bpm": 176,

e6fe365f072426c32a3892351519bbb872588449
Chris <caschoener@gmail.com>
2022-08-28 18:39:19 -0400

    ugly merge

--- generate_show.py
+++ generate_show.py
@@ -8,30 +8,28 @@ import numpy as np
 from scipy.signal import find_peaks
 import pandas as pd 
 import aubio
-import numpy as np
+
 from helpers import *
 
 
 def generate_show(song_filepath):
     print(f'{bcolors.OKGREEN}Generating show for "{song_filepath}"{bcolors.ENDC}')
 
-
-    # -------------- tempo and offset -------------------
+    # internet copypasta from here ...
     win_s = 512                 # fft size
     hop_s = win_s // 2          # hop size
-
-    print(f'song_filepath: {song_filepath}')
+    
     src = aubio.source(str(song_filepath), 0, hop_s)
 
     print(src.uri, src.samplerate, src.channels, src.duration)
     o = aubio.tempo("default", win_s, hop_s, src.samplerate)
+
     delay = 4. * hop_s
 
     beats = []
 
     # total number of frames read
     total_frames = 0
-    bpms = []
     while True:
         samples, read = src()
         is_beat = o(samples)
@@ -40,7 +38,6 @@ def generate_show(song_filepath):
             human_readable = (this_beat / float(src.samplerate))
             # print("%f" % human_readable)
             beats.append(human_readable)
-            bpms.append(o.get_bpm())
         total_frames += read
         if read < hop_s: break
     # ...to here
@@ -72,8 +69,10 @@ def generate_show(song_filepath):
     length_int = 60.0/bpm_guess
     delay = length_int - offset_guess if offset_guess > 0 else -offset_guess
 
+    print(f'Guessing BPM as {bpm_guess} delay as {delay} beat_length as {length_int}')
+
     show = {
-        'bpm': int(bpm),
+        'bpm': int(bpm_guess),
         'song_path': str(song_filepath),
         'delay_lights': delay,
         'skip_song': 0.0,
@@ -81,13 +80,14 @@ def generate_show(song_filepath):
         'beats': []
     }
 
-    # -------------- output ---------------------
+    # apply lights
     modes_to_cycle = ['Red top', 'Green top']
     length_s = src.duration/src.samplerate
-    total_beats = int(length_s/60*bpm)
+    total_beats = int(length_s/60*bpm_guess)
     for beat in range(1, total_beats):
         mode = modes_to_cycle[beat % len(modes_to_cycle)]
         show['beats'].append([beat, mode, .25])
     return {
         f'generated_{pathlib.Path(song_filepath).stem}_show': show
     }
+

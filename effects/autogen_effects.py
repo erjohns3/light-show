c389bf4fce01e11cb58248890644dafc6409be67
curtailing returns <gamechefproductions@gmail.com>
2022-08-29 19:14:07 -0400

    sad

---
+++ effects/autogen_effects.py
@@ -0,0 +1,35 @@
+effects = {
+    "Autogen Strobe": {
+        "length": 1,
+        "autogen": True,
+        "beats": [
+            [1, 'Strobe', 1],
+        ],
+    },
+    "Autogen UV Strobe": {
+        "length": 1,
+        "autogen": True,
+        "beats": [
+            [1, 'UV Strobe', 1],
+        ],
+    },
+    "Autogen bassline": {
+        "length": 16,
+        "autogen": True,
+        "beats": [
+            [1, "Purple bottom", 1, 0.42, .2],
+            [2, "Green bottom", 1, 0.42, .2],
+        ],
+    },
+    "Autogen kicks": {
+        "length": 16,
+        "autogen": True,
+        "beats": [
+            [1, "Green bottom", 0.1, 0.6, 0.17],
+            [1.1, "Green bottom", 3, 0.17, 0.17],
+            [2, "Sidechain bottom g", 0.5, 1, 0], 
+            [2, "Some color bottom", 0.3, 1, .7],
+        ],
+    },
+
+}

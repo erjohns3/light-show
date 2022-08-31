27b79681d1ad2a849257ea81625037acf3221468
Andrew Duerig <gamechefproductions@gmail.com>
2022-08-31 14:02:23 -0400

    admin update

--- users.py
+++ users.py
@@ -3,4 +3,16 @@ users = {
         "name": "Eric Laptop",
         "admin": True
     },
+    "82c4e8e0-644d-cd3c-cac6-8875aace6356": {
+        "name": "Andrews Upstairs Computer Chrome",
+        "admin": True
+    },
+    "f9219d16-42e7-bf2f-9fa6-a2828d87bdc": {
+        "name": "Andrews Upstairs Computer Chrome",
+        "admin": True
+    },
+    "65f9722b-2f4-fb71-e368-748a23def1a8": {
+        "name": "Andrews Upstairs Computer Firefox",
+        "admin": True
+    },
 }

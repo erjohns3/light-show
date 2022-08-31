666b1c527ea82f50915eddcc3598db914abab5b0
curtailing returns <gamechefproductions@gmail.com>
2022-08-31 19:06:22 -0400

    Yeah

---
+++ effects/andrew_effects.py
@@ -0,0 +1,67 @@
+effects = {
+    "A - only Flash Top": {
+        "length": 0.2,
+        "trigger": "hold",
+        "profiles": ['Andrew'],
+        "beats": [
+            [1, [100, 100, 100, 0, 0, 0, 0], 0.07],
+            [1.07, [-1000, -1000, -1000, -1000, 1000, -1000, 0], 2],
+        ],
+    },
+    "A - only UV": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, -1000, -1000, -1000, -1000, -1000, 100], 1],
+        ],
+    },
+    "A - only Red top": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [100, -1000, -1000, -1000, -1000, -1000, 0], 1],
+        ],
+    },
+    "A - only Green top": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, 100, -1000, -1000, -1000, -1000, 0], 1],
+        ],
+    },
+    "A - only Blue top": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, -1000, 100, -1000, -1000, -1000, 0], 1],
+        ],
+    },
+    "A - only Red bottom": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, -1000, -1000, 100, -1000, -1000, 0], 1],
+        ],
+    },
+    "A - only Green bottom": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, -1000, -1000, -1000, 100, -1000, 0], 1],
+        ],
+    },
+    "A - only Blue bottom": {
+        "length": 1,
+        "trigger": "hold",
+        "profiles": ["Andrew"],
+        "beats": [
+            [1, [-1000, -1000, -1000, -1000, -1000, 100, 0], 1],
+        ],
+    },
+}

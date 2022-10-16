c2b9aad4c26bc44b7e08f968bc5d7b3b5c844bc9
Andrew Duerig <gamechefproductions@gmail.com>
2022-10-16 14:23:16 -0400

    moving around files

--- tagger.py
+++ tagger.py
@@ -37,3 +37,37 @@ else:
                 print('saving...')
                 os.system(f'ffmpeg -i "{filepath}" -metadata title="{title}" -metadata artist="{artist}" -c:a copy tmp.ogg')
                 os.system(f'mv tmp.ogg "{filepath}"')
+
+
+
+
+
+# andrews file
+# import music_tag
+
+# from helpers import * 
+
+# # can do the same thing with tags['artist'] too
+
+# def assign_tag(filepath, tags, tag_name):
+#     if not tags[tag_name]:
+#         print_red(f'NOT SET {tag_name} - {filepath.name}')
+#         new_tag = input().strip()
+#         if new_tag:
+#             tags[tag_name] = new_tag
+#             print_green(f'{tag_name} - {tags[tag_name]}: "{filepath.name}"')
+#             tags.save()
+#         else:
+#             print('Entered nothing, skipping file')
+#     else:
+#         print_green(f'{tag_name} - {tags[tag_name]}: "{filepath.name}"')
+
+
+# def assign_title_tag_if_not_exist(filepath):
+#     if filepath.suffix in ['.mp3', '.ogg', '.wav']:
+#         tags = music_tag.load_file(filepath)
+#         assign_tag(filepath, tags, 'title')
+#         assign_tag(filepath, tags, 'artist')
+
+# for name, path in get_all_paths('songs', only_files=True):
+#     assign_title_tag_if_not_exist(path)

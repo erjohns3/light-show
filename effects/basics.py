bd9cb438b76d5ce1d279a04e8ec9762185fdb696
Your Name <you@example.com>
2022-10-22 19:56:13 -0400

    5 nights

--- effects/basics.py
+++ effects/basics.py
@@ -81,6 +81,12 @@ effects = {
             [1, [-1000, -1000, -1000, 0, 0, 0, 0], 1],
         ],
     },
+    "Sidechain bottom rbg": {
+        "length": 1,
+        "beats": [
+            [1, [0, 0, 0, -1000, -1000, -1000, 0], 1],
+        ],
+    },
     "Sidechain bottom g": {
         "length": 1,
         "beats": [
--- effects/five_nights.py
+++ effects/five_nights.py
@@ -119,14 +119,78 @@ effects = {
     },
 
     "five nights chorus": {
-        "length": 50,
+        "length": 64,
         "beats": [
+            [1, "five nights static bottom", 16, .6, .6],
             [1, "five nights green red top", 7],
             [9, "five nights red green top", 7],
             [17, "five nights red circle", 8],
-            [25, "five nights green circle 2", 8],
-            [33, "five nights red circle 2", 8],
-            [41, "five nights green circle", 8],
+            [25, "five nights green circle 2", 16],
+            [33, "five nights red circle 2", 8, 1],
+            [41, "five nights static bottom", 8],
+            [41, "five nights static top", 8],
+            #  quarter notes after chorus
+            [49, "rainbow good top", 16]
+        ],
+    },
+
+     "five nights static bottom": {
+        "length": 11,
+        "beats": [
+            [1, "White bottom", 11, 1, 1],
+            [2.9, "Sidechain bottom rbg", .1 ],
+            [3.2, "Sidechain bottom rbg", .1 ],
+            [3.6, "Sidechain bottom rbg", .1 ],
+            [4.1, "Sidechain bottom rbg", .1 ],
+            [5.1, "Sidechain bottom rbg", .1],
+            [5.5, "Sidechain bottom rbg", .2],
+            [6, "Sidechain bottom rbg", .1],
+            [6.2, "Sidechain bottom rbg", .1],
+            [6.5, "Sidechain bottom rbg", .1],
+            [7.3, "Sidechain bottom rbg", .1],
+            [7.7, "Sidechain bottom rbg", .2],
+            [8.1, "Sidechain bottom rbg", .1],
+            [8.7, "Sidechain bottom rbg", .2],
+            [8.97, "Sidechain bottom rbg", .1],
+            [9.4, "Sidechain bottom rbg", .2],
+            [9.9, "Sidechain bottom rbg", .1],
+            [10.3, "Sidechain bottom rbg", .1],
+            [10.7, "Sidechain bottom rbg", .1],
+            [11.2, "Sidechain bottom rbg", .2],
+        ],
+    },
+
+    "five nights static top": {
+        "length": 11,
+        "beats": [
+            [1, "White top", 11, 1, 1],
+            [2.9, "Sidechain top rbg", .1 ],
+            [3.2, "Sidechain top rbg", .1 ],
+            [3.6, "Sidechain top rbg", .1 ],
+            [4.1, "Sidechain top rbg", .1 ],
+            [5.1, "Sidechain top rbg", .1],
+            [5.5, "Sidechain top rbg", .2],
+            [6, "Sidechain top rbg", .1],
+            [6.2, "Sidechain top rbg", .1],
+            [6.5, "Sidechain top rbg", .1],
+            [7.3, "Sidechain top rbg", .1],
+            [7.7, "Sidechain top rbg", .2],
+            [8.1, "Sidechain top rbg", .1],
+            [8.7, "Sidechain top rbg", .2],
+            [8.97, "Sidechain top rbg", .1],
+            [9.4, "Sidechain top rbg", .2],
+            [9.9, "Sidechain top rbg", .1],
+            [10.3, "Sidechain top rbg", .1],
+            [10.7, "Sidechain top rbg", .1],
+            [11.2, "Sidechain top rbg", .2],
+        ],
+    },
+
+    "five nights green red bottom": {
+        "length": 2,
+        "beats": [
+            [1, "Red bottom", .9],
+            [2, "Green bottom", .9],
         ],
     },
 
@@ -137,9 +201,10 @@ effects = {
         "skip_song": 0.0,
         "beats": [
             # quiet intro
-            [1,"five nights melody 1 intro", 16],
-            [1, "torreador", 49, .1, .5],
-            # Getting louder
+            [1,"five nights melody 1 intro", 16, .4, .4],
+            [1, "torreador", 16, .1, .5],
+            # Verse
+            [17, "torreador", 33, .8],
             [17, "five nights pulse red", 32],
             # Build up to chorus
             [49, "five nights bass 1", 31],
@@ -148,7 +213,26 @@ effects = {
             [65, "UV pulse", 15],
             # (one beat dark)
             # Chorus
-            [81, "five nights chorus", 50]
+            [81, "five nights chorus", 64],
+            # Verse
+            [145, "torreador", 33, .8],
+            [145, "five nights pulse red", 32],
+            # Build up to chorus
+            [177, "five nights bass 1", 31],
+            [177, "UV Strobe", 16],
+            # half way through chorus
+            [193, "UV pulse", 15],
+             # (one beat dark)
+            # Chorus
+            [209, "five nights chorus", 40],
+            [249, "five nights static top", 24, .5, .5, 3],
+            [249, "five nights green red bottom", 22],
+            # Scream thing at the end
+            [273, "Red top", 16, 1, 1],
+            [273, "Red bottom", 16, 1, 1],
+            [273, "UV", 16, 1, 1],
         ]
     }
 }
+
+# [beat, effect, length, start intensity, end intensity, beat skip, hue[-1, 1], sat, brightness]

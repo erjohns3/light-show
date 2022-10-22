ff25ef7462a60787c717169ed6b49ff83079fe8c
Eric Johnson <johnson.eric321@gmail.com>
2022-10-22 02:13:36 -0400

    am

--- effects/black_hole.py
+++ effects/black_hole.py
@@ -1,7 +1,7 @@
 effects = {
     "Muse  Supermassive Black Hole Official Music Video show": {
         "bpm": 120,
-        "song_path": "songs\\Muse  Supermassive Black Hole Official Music Video.ogg",
+        "song_path": "songs/Muse  Supermassive Black Hole Official Music Video.ogg",
         "delay_lights": 0.154,
         "skip_song": 0.0,
         "beats": [
--- effects/hot_milk.py
+++ effects/hot_milk.py
@@ -1,7 +1,7 @@
 effects = {
     "Snails House  Hot Milk show": {
         "bpm": 150,
-        "song_path": "songs\\Snails House  Hot Milk.ogg",
+        "song_path": "songs/Snails House  Hot Milk.ogg",
         "delay_lights": 0.02055,
         "skip_song": 0.0,
         "beats": []

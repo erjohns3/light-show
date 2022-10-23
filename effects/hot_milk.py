74f4689ff434a31f3cae7d9b68f150f08582378e
Andrew Duerig <gamechefproductions@gmail.com>
2022-10-23 04:45:32 -0400

    done

--- effects/hot_milk.py
+++ effects/hot_milk.py
@@ -6,6 +6,8 @@ effects = {
         "song_path": "songs/Snails House  Hot Milk.ogg",
         "delay_lights": 0.02055,
         "skip_song": 0.0,
-        "beats": []
+        "beats": [
+            beat(1, name='porter flubs phrase', length=64, hue_shift=.7, intensity=(1, 0)),
+        ]
     }
 }

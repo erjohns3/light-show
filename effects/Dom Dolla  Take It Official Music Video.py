a41a95c649b4eafb267d395a1511f106adc58379
Your Name <you@example.com>
2022-10-26 00:37:42 -0400

    yea

---
+++ effects/Dom Dolla  Take It Official Music Video.py
@@ -0,0 +1,11 @@
+from effects.compiler import b
+
+effects = {
+    "Dom Dolla  Take It Official Music Video show": {
+        "bpm": 123,
+        "song_path": "songs\\Dom Dolla  Take It Official Music Video.ogg",
+        "delay_lights": 0.15050000000000002,
+        "skip_song": 0.0,
+        "beats": []
+    }
+}

e77e127982d39930e73f2f8abe220f1286f956e5
meabbott714 <meabbott714@gmail.com>
2022-10-26 01:04:46 -0400

    start of control

--- effects/control.py
+++ effects/control.py
@@ -1,6 +1,55 @@
 from effects.compiler import b
 
 effects = {
+
+    "control UV clap": {
+        "length": 2,
+        "beats": [
+            [2, "UV", 1, 1, 0],
+        ],
+    },
+
+    "control cycle bottom": {
+        "length": 32,
+        "beats": [
+            [1, "Red bottom", 8],
+            [1, "Blue bottom", 8, .2, 1],
+            [9, "Red bottom", 8, 1, .1],
+            [9, "Blue bottom", 8],
+            [17, "Red bottom", 8, .1, 1],
+            [17, "Blue bottom", 8],
+            [25, "Red bottom", 8,],
+            [25, "Blue bottom", 8, 1, .2],
+        ],
+    },
+
+    "control pulse rb": {
+        "length": 1,
+        "beats": [
+            [1, "Red bottom", 1, 1, 0],
+            [1, "Blue bottom", 1, 1, 0],
+        ],
+    },
+
+    "control cycle bottom pulse": {
+        "length": 32,
+        "beats": [
+            [1, "control cycle bottom", 32, .3],
+            [1, "control pulse rb", 8, .1, .4],
+            [9, "control pulse rb", 8, .4, .1],
+            [17, "control pulse rb", 8, .1, .4],
+            [25, "control pulse rb", 8, .4, .1],
+        ],
+    },
+
+    "control UV clap full": {
+        "length": 16,
+        "beats": [
+            [1, "control UV clap", 16],
+            [15.5, "UV", .4, 1, 0],
+        ],
+    },
+
     "control top step fade": {
         "length": 4,
         "beats": [
@@ -29,12 +78,29 @@ effects = {
         ],
     },
 
+    "control bottom rainbow": {
+        "length": 4,
+        "beats": [
+            [1, "Red top", 4, 1, 0],
+            [2,  "control subtract 20"],
+            [2.5,  "control subtract 20"],
+            [2.7,  "control subtract 20"],
+            [3,  "control subtract 20"],
+        ],
+    },
+
     "control intro no bass": {
         "length": 64,
         "beats": [
-            [1, "control top step fade", 16, 1, 1, 0, 267/360],
-            [17, "control top step fade", 16, 1, 1, 0, 267/360],
-            [30, "control top step fade", 2, 1, 1, 0, 300/360],
+            [1, "control top step fade", 4, 1, 1, 0, 267/360],
+            [9, "control top step fade", 4, 1, 1, 0, 267/360],
+            [17, "control top step fade", 4, 1, 1, 0, 285/360],
+            [25, "control top step fade", 4, 1, 1, 0, 285/360],
+            [30, "control top flicker fade", 3, 1, 1, 0, 300/360],
+            [33, "control top step fade", 4, 1, 1, 0, 267/360],
+            [41, "control top step fade", 4, 1, 1, 0, 267/360],
+            [49, "control top step fade", 4, 1, 1, 0, 285/360],
+            [57, "control top step fade", 4, 1, 1, 0, 285/360],
         ],
     },
 
@@ -44,7 +110,9 @@ effects = {
         "delay_lights": 0.055,
         "skip_song": 0.0,
         "beats": [
-            [1, "control intro no bass", 100],
+            [1, "control intro no bass", 128],
+            [65, "control UV clap full", 64],
+            [65, "control cycle bottom pulse", 64, 1, 1, 0, -.05],
         ]
     }
 }

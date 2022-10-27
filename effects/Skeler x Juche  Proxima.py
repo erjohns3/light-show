10a57ebf3e3389968b27a23812f4b426917c6ece
Eric Johnson <johnson.eric321@gmail.com>
2022-10-27 01:01:16 -0400

    chunk

---
+++ effects/Skeler x Juche  Proxima.py
@@ -0,0 +1,11 @@
+from effects.compiler import b
+
+effects = {
+    "Skeler x Juche  Proxima show": {
+        "bpm": 130,
+        "song_path": "songs\\Skeler x Juche  Proxima.ogg",
+        "delay_lights": -0.0,
+        "skip_song": 0.0,
+        "beats": []
+    }
+}

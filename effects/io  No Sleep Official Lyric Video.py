0758050d83a4a13e1f285fa4d51dabf6cab4e595
curtailing returns <gamechefproductions@gmail.com>
2022-10-27 01:01:48 -0400

    song

--- effects/io  No Sleep Official Lyric Video.py
+++ effects/io  No Sleep Official Lyric Video.py
@@ -3,16 +3,17 @@ from effects.compiler import b
 effects = {
     "no sleep show": {
         "bpm": 125,
-        "song_path": "songs\\io  No Sleep Official Lyric Video.ogg",
+        "song_path": "songs/io  No Sleep Official Lyric Video.ogg",
         "delay_lights": 0.25849999999999995,
         "skip_song": 0.0,
+        "profiles": ["Eric"],
         "beats": [
             [4, 'Red top', 8, 1, 0],
-            [4, 'Red disco', 160, 0, 1],
+            [4, 'Red disco', 144, 0, 1],
             [164, 'Red bottom pulse', 96],
             [164.5, 'Red disco pulse', 96],
             [260, 'Red top', 8, 1, 0],
-            [260, 'Red disco', 140, 0, 1],
+            [260, 'Red disco', 128, 0, 1],
             [400, 'Red bottom pulse', 96],
             [400.5, 'Red disco pulse', 96],
         ]
@@ -31,7 +32,7 @@ effects = {
                     0,
                     0, 0, 0,
                     0, 0, 0
-                ], 1, 1, 0],
+                ], 0.25],
         ]
     },
 
@@ -52,13 +53,47 @@ effects = {
         ]
     },
 
+    "Green disco": {
+        "length": 1,
+        "trigger": "toggle",
+        "loop": True,
+        "profiles": ["Eric"],
+        "beats": [
+            [1, [
+                    0, 0, 0, 
+                    0, 0, 0, 
+                    0, 0, 0, 
+                    0,
+                    0, 0, 0,
+                    0, 100, 0
+                ], 1],
+        ]
+    },
+
+    "Blue disco": {
+        "length": 1,
+        "trigger": "toggle",
+        "loop": True,
+        "profiles": ["Eric"],
+        "beats": [
+            [1, [
+                    0, 0, 0, 
+                    0, 0, 0, 
+                    0, 0, 0, 
+                    0,
+                    0, 0, 0,
+                    0, 0, 100
+                ], 1],
+        ]
+    },
+
     "Red disco pulse": {
         "length": 1,
         "trigger": "toggle",
         "loop": True,
         "profiles": ["Eric"],
         "beats": [
-            [1, 'Red disco', 1, 1, 0],
+            [1, 'Red disco', 0.25],
         ]
     },
 }

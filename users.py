eebef7cde77c21541b2d4f4a2b42e36238dc32f9
Andrew <duerig.andrew@gmail.com>
2023-01-08 17:14:53 -0500

    bug fix

--- users.py
+++ users.py
@@ -1,55 +1,58 @@
 users = {
     "50b0dd6c-75e1-5729-c96f-3af0198ad12a": {
         "name": "David or Chris",
-        "admin": True
+        "admin": True,
     },
     "2155c34-6648-d4d8-12a0-d6d2429d5030": {
         "name": "David or Chris 2",
-        "admin": True
+        "admin": True,
     },
     "2a61fedc-b7e8-7ab1-578e-9c4461818c41": {
         "name": "Eric Laptop",
-        "admin": True
+        "admin": True,
     },
     "6710fac3-61fb-a35e-9bab-c058ec6ed25c": {
         "name": "Matts Phone Chrome",
-        "admin": True
+        "admin": True,
     },
     "82c4e8e0-644d-cd3c-cac6-8875aace6356": {
         "name": "Andrews Upstairs Computer Chrome",
-        "admin": True
+        "admin": True,
     },
     "e465a27f-1e8f-ca37-2ad3-c148ebe1e54a": {
         "name": "Andrews Upstairs Computer Firefox",
-        "admin": True
+        "admin": True,
     },
     "f9219d16-42e7-bf2f-9fa6-a2828d87bdc": {
         "name": "Andrews Phone",
-        "admin": True
+        "admin": True,
     },
     "af24e5b0-4bc8-fc43-1a6f-9f54a4d3a76a": {
         "name": "Andrews Phone 2",
-        "admin": True
+        "admin": True,
     },
     "f03bcee9-b573-338f-9289-a49ebd70f062": {
         "name": "Erics phone",
-        "admin": True
+        "admin": True,
     },
     "65f9722b-2f4-fb71-e368-748a23def1a8": {
         "name": "Andrews Upstairs Computer Firefox",
-        "admin": True
+        "admin": True,
     },
     "1222071-a256-95f4-9f8c-24d79da3c4b": {
         "name": "Chrises phone",
-        "admin": True
+        "admin": True,
     },
     "92e6ffd2-63df-e664-2663-12fe513d3e43": {
         "name": "Ray phone",
-        "admin": True
+        "admin": True,
     },
     "dbbf9427-3cdf-e939-c8ed-769c2bbde0b1": {
         "name": "Ray firefox",
-        "admin": True
+        "admin": True,
     },
-    
+    "bc43fa74-ab14-fe59-698c-25294c2a1293": {
+        "name": "Andrews laptop chrome",
+        "admin": True,
+    },
 }

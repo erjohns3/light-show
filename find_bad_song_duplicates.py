3ec768a4ab8b793f536b8d0680f01da3d88b635c
Andrew Duerig <gamechefproductions@gmail.com>
2023-01-12 03:41:38 -0500

    uyes

--- find_bad_song_duplicates.py
+++ find_bad_song_duplicates.py
@@ -15,7 +15,6 @@ ray_songs_folder = get_ray_directory().joinpath('music_creation').joinpath('down
 songs_folder = pathlib.Path(__file__).resolve().parent.joinpath('songs')
 print(f'Scanning {ray_songs_folder} and {songs_folder} for duplicates...')
 for name2, path2 in get_all_paths(ray_songs_folder, only_files=True, recursive=True, allowed_filepaths=allowed_filepaths):
-    # print('scanning', path2, name2)
     for name1, path1 in get_all_paths(songs_folder, only_files=True, recursive=True, allowed_filepaths=allowed_filepaths):
         if path1.stem != path2.stem and is_similar(path1.stem, path2.stem):
             print(f'TOO SIMILAR:\n    {blue(path1.stem)} - {path1}\n    {yellow(path2.stem)} - {path2}')

11a6a6cf9b2ce9179756e45f52c582e272404cea
Andrew Duerig <gamechefproductions@gmail.com>
2023-01-12 03:20:10 -0500

    ok

--- find_bad_song_duplicates.py
+++ find_bad_song_duplicates.py
@@ -1,6 +1,22 @@
+from thefuzz import fuzz, process
+
 from helpers import *
 
 
+def is_similar(item1, item2):
+    return fuzz.ratio(item1, item2) > 30
 
-for name, path in get_all_paths(only_files=True, recursive=True, allowed_filepaths=['.wav', '.ogg']):
-    print(blue(name), path)
+def fuzzy_find_get_one(search, collection):
+    return process.extractOne(search, collection)[0]
+
+
+allowed_filepaths = set(['.wav', '.ogg'])
+ray_songs_folder = get_ray_directory().joinpath('music_creation').joinpath('downloaded_songs')
+songs_folder = pathlib.Path(__file__).resolve().parent.joinpath('songs')
+print(f'Scanning {ray_songs_folder} and {songs_folder} for duplicates...')
+for name2, path2 in get_all_paths(ray_songs_folder, only_files=True, recursive=True, allowed_filepaths=allowed_filepaths):
+    print(name2)
+    for name1, path1 in get_all_paths(songs_folder, only_files=True, recursive=True, allowed_filepaths=allowed_filepaths):
+        print(name1)
+        if is_similar(name1, name2):
+            print('TOO SIMILAR:', blue(path1), yellow(path2))

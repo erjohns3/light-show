9c7b63f85663d453999c06d0e463b87ab254cd30
meabbott714 <meabbott714@gmail.com>
2023-01-16 14:27:42 -0500

    Started Vitas 7th Element

---
+++ effects/vitas7thElement.py
@@ -0,0 +1,16 @@
+from effects.compiler import b
+
+
+# 1-11 intro
+# 12 - Starting beat
+effects = {
+    "Vitas - The 7th Element": {
+        "bpm": 128,
+        "song_path": "songs/Vitas - The 7th Element.ogg",
+        "delay_lights": 0.16799999999999998,
+        "skip_song": 0.0,
+        "beats": [
+            b(1, name='im blue pulse top', length=180),   
+        ]
+    }
+}

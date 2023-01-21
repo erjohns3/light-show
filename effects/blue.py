67c357b40c4e1edcdecadc816e3894d973588dd3
Your Name <you@example.com>
2023-01-20 19:20:13 -0500

    asd

--- effects/blue.py
+++ effects/blue.py
@@ -519,9 +519,9 @@ effects = {
         ],
     },
 
-    "Eiffel 65  Blue Flume Remix  Official Visualiser": {
+    "Eiffel 65 - Blue (Da Ba Dee) [zA52uNzx7Y4].mp3": {
         "bpm": 135,
-        "song_path": "songs/Eiffel 65  Blue Flume Remix  Official Visualiser.ogg",
+        "song_path": "songs/Eiffel 65 - Blue (Da Ba Dee) [zA52uNzx7Y4].ogg",
         "delay_lights": 0.0228,
         "skip_song": 0.0,
         "beats": [

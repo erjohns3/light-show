145a16d8993091d65d10cae2aeebf3193adb94fc
Emma Pannullo <emma.pannullo@tufts.edu>
2023-06-19 10:25:54 -0400

    Add new song

---
+++ effects/ROSALÍA - SAOKO (Apolø Remix).py
@@ -0,0 +1,15 @@
+from effects.compiler import b
+
+effects = {
+    
+    "ROSALÍA - SAOKO (Apolø Remix)": {
+        "bpm": 118,
+        "song_path": "songs/ROSALÍA - SAOKO (Apolø Remix).ogg",
+        "delay_lights": 0.0,
+        "skip_song": 0.0,
+        "beats": [
+
+            
+        ]
+    }
+}

9dd71cb3e63ae651654c4985a46f49f0e092ef33
Emma Pannullo <emma.pannullo@tufts.edu>
2023-06-24 13:43:10 -0400

    Update I Follwo Rivers

--- effects/Lykke Li - I Follow Rivers (The Magician Remix).py
+++ effects/Lykke Li - I Follow Rivers (The Magician Remix).py
@@ -11,10 +11,10 @@ effects = {
     "ll fill" : {
         "length": 6,
         "beats": [
-            b(1, name='Cyan top', length=1.5),
-            b(2.5, name='Pink top', length=0.5),
-            b(3, name='Purple top', length=0.75),
-            b(3.75, name='Blue top', length=2.25)
+            b(1, name='Cyan top', length=1.5, sat_shift=-.25, intensity=(1, 0)),
+            b(2.5, name='Pink top', length=0.5, sat_shift=-.25, intensity=(1, 0)),
+            b(3, name='Purple top', length=0.75, sat_shift=-.25, intensity=(1, 0)),
+            b(3.75, name='Blue top', length=2.75, sat_shift=-.25, intensity=(1, 0))
         ]
     },
     "dom sidechain motor halfs": {
@@ -23,20 +23,30 @@ effects = {
             b(1.15, name='Sidechain motor', length=.85),
         ]
     },
-    "ll chorus laser": {
-        'length': 16,
+    "ll chorus laser green": {
+        'length': 8,
         'autogen': 'laser long',
         "beats": [
-            b(1, name='laser motor', length=16),
-            b(1, name='dom sidechain motor halfs', length=16),
-            b(1, name='green laser', length=16),
+            b(1, name='laser motor', length=8),
+            b(1, name='dom sidechain motor halfs', length=8),
+            b(1, name='green laser', length=8),
+        ]
+    },
+    "ll chorus laser red": {
+        'length': 8,
+        'autogen': 'laser long',
+        "beats": [
+            b(1, name='laser motor', length=8),
+            b(1, name='dom sidechain motor halfs', length=8),
+            b(1, name='red laser', length=8),
         ]
     },
     "ll chorus": {
         'length': 16,
         'autogen': 'laser long',
         "beats": [
-            b(1, name='ll chorus laser', length=16)
+            b(1, name='ll chorus laser green', length=8),
+            b(9, name='ll chorus laser red', length=8)
         ]
     },
     "ll pre chorus": {
@@ -59,6 +69,24 @@ effects = {
             b(1, name='Indigo bottom', length=4, sat_shift=-.25, intensity=(1, 0)),
         ],
     },
+    "verse disco" : {
+        "length": 8,
+        "beats": [
+            b(1, name='Blue disco pulse', length=2),
+            b(2.5, name='Blue disco pulse', length=1),
+            b(3.5, name='Blue disco pulse', length=1),
+            b(5, name='Blue disco pulse', length=2),
+            b(6.5, name='Blue disco pulse', length=1),
+            b(7.5, name='Blue disco pulse', length=1)
+    
+        ]
+    },
+    "bridge disco" : {
+        "length": 10,
+        "beats": [
+            
+        ]
+    },
     "Lykke Li - I Follow Rivers (The Magician Remix)": {
         "bpm": 122,
         "song_path": "songs/Lykke Li - I Follow Rivers (The Magician Remix).ogg",
@@ -68,7 +96,7 @@ effects = {
             b(9, name='ll bass', length=144),
             b(36, name='ll fill', length=6),
             b(68, name='ll fill', length=6),
-            b(73, name='Blue disco', length=72),
+            b(73, name='verse disco', length=72),
             b(84, name='ll fill', length=6),
             b(100, name='ll fill', length=6),
             b(116, name='ll fill', length=6),
@@ -86,13 +114,16 @@ effects = {
             b(265, name='ll bridge', length=48),
             b(276, name='ll fill', length=6),
             b(292, name='ll fill', length=6),
-            b(313, name='Blue disco', length=32),
-            b(345, name='Blue disco pulse', length=64),
+            b(313, name='Blue disco pulse', length=32),
+            b(345, name='Rgb disco pulse', length=32),
+            b(380, name='Strobe bottom', length=28, sat_shift=.2, intensity=(0, 1)),
             b(372, name='ll fill', length=6),
             b(388, name='ll fill', length=6),
             b(404, name='ll fill', length=6),
             b(409, name='ll chorus', length=160),
+            b(411, name='UV pulse', length=58),
             b(471, name='ll clap', length=2),
+            b(473, name='UV pulse', length=220),
             b(409, name='ll bass', length=160)
 
 

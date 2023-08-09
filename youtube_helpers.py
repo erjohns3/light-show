e7caf796da42d1b6b58cbf98bb7c8cf42a16e46b
Chris <caschoener@gmail.com>
2023-08-09 11:13:06 -0400

    force everyone to update yt-dlp

--- youtube_helpers.py
+++ youtube_helpers.py
@@ -35,6 +35,8 @@ results_to_consider = 3
 def youtube_search(search_phrase, minimum_length_of_video_seconds=50, maximum_length_of_video_seconds=1000):
     global last_youtube_searched_time
     import yt_dlp
+    if yt_dlp.version.__version__ < 'yt-dlp-2023.7.6':
+        raise Exception(f'Please run the command `pip install -r yt-dlp`')
 
     if last_youtube_searched_time:
         time_to_wait = max(0, minimum_youtube_api_wait_time_seconds - (time.time() - last_youtube_searched_time))

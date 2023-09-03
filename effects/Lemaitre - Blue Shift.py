a0a844149a8103cb7245efeae87e34293eb5adbc
Your Name <you@example.com>
2023-09-03 12:21:12 -0400

    Create twinkle effect

---
+++ effects/Lemaitre - Blue Shift.py
@@ -0,0 +1,49 @@
+import random
+from copy import deepcopy
+
+from effects.compiler import *
+
+import grid_helpers
+
+# Set percentage of grid to twinkle
+TWINKLE_PERCENT = 0.1
+
+# Find number of random points on grid to make twinkle
+NUM_GRID_TWINKLE = int(TWINKLE_PERCENT * (grid_helpers.GRID_WIDTH * grid_helpers.GRID_HEIGHT))
+
+# Total number of beats for twinkle effect
+TWINKLE_TIME = 100
+
+# Time for each twinkle to fade
+TWINKLE_FADE = 2
+
+beats = []
+
+for i in range(1, TWINKLE_TIME, TWINKLE_FADE):
+    for j in range(int(NUM_GRID_TWINKLE)):
+        # Get random point on grid
+        x = random.randint(-(grid_helpers.GRID_WIDTH/2), (grid_helpers.GRID_WIDTH/2))
+        y = random.randint(-(grid_helpers.GRID_HEIGHT/2), (grid_helpers.GRID_HEIGHT/2))
+        # Create random offset for start beat of each twinkle
+        t_offset = random.uniform(0, 2)
+        beats.append(
+                grid_f(
+                    i + t_offset,
+                    function=our_transform,
+                    object=get_rectangle_numpy(1, 1),
+                    start_pos=(y,x),
+                    start_color=(100,100,100),
+                    end_color=(0, 0, 0),
+                    length=TWINKLE_FADE,
+                )
+            )
+
+effects = {
+    "Lemaitre - Blue Shift": {
+        "bpm": 118,
+        "song_path": "songs/Lemaitre - Blue Shift.ogg",
+        "delay_lights": 0.4043245762711864,
+        "skip_song": 0.0,
+        "beats": beats,
+    }
+}

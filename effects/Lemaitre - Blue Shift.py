5a97122c04492804a125087b5f0e712bc84e5579
Andrew Duerig <gamechefproductions@gmail.com>
2023-09-10 10:45:16 -0400

    asd

--- effects/Lemaitre - Blue Shift.py
+++ effects/Lemaitre - Blue Shift.py
@@ -1,4 +1,5 @@
 import random
+from collections import deque
 
 from effects.compiler import *
 
@@ -51,6 +52,7 @@ def random_color():
     r = random.randint(0, 100)
     return (b, g, r)
 
+
 def trail_ball(grid_info):
     if getattr(grid_info, 'pos', None) is None:
         grid_info.pos = (random.randint(0, grid_helpers.GRID_WIDTH - 1), random.randint(0, grid_helpers.GRID_HEIGHT - 1))
@@ -80,6 +82,50 @@ def trail_ball(grid_info):
         grid_helpers.grid[grid_info.pos[0]][grid_info.pos[1]] = grid_info.color
 
 
+def minus_color(color, amt):
+    return (max(color[0] - amt, 0), max(color[1] - amt, 0), max(color[2] - amt, 0))
+
+def trail_ball_fade(grid_info):
+    if getattr(grid_info, 'pos', None) is None:
+        grid_info.pos = (random.randint(0, grid_helpers.GRID_WIDTH - 1), random.randint(0, grid_helpers.GRID_HEIGHT - 1))
+        grid_info.dir = (1 - (random.randint(0, 1) * 2), 1 - (random.randint(0, 1) * 2))
+        grid_info.color = random_color()
+        grid_info.trail = deque([])
+    speed = int(1 / getattr(grid_info, 'speed', 1))
+
+    if grid_info.curr_sub_beat % speed == 0:
+        x, y = grid_info.pos
+        d_x, d_y = grid_info.dir
+
+        if x + d_x < 0 or x + d_x >= grid_helpers.GRID_WIDTH:
+            grid_info.dir = (grid_info.dir[0] * -1, grid_info.dir[1])
+            d_x *= -1
+            grid_info.color = random_color()
+
+        if y + d_y < 0 or y + d_y >= grid_helpers.GRID_HEIGHT:
+            grid_info.dir = (grid_info.dir[0], grid_info.dir[1] * -1)
+            d_y *= -1
+            grid_info.color = random_color()
+
+        index = 0
+        while index < len(grid_info.trail):
+            (p_x, p_y), p_color = grid_info.trail[index]
+            
+            p_color = minus_color(p_color, 5)
+
+            grid_info.trail[index][1] = p_color
+            
+            grid_helpers.grid[p_x][p_y] = p_color
+            if p_color == (0, 0, 0) and index == 0:
+                grid_info.trail.popleft()
+            else:
+                index += 1
+        grid_info.pos = (x + d_x, y + d_y)
+        grid_helpers.grid[grid_info.pos[0]][grid_info.pos[1]] = grid_info.color
+
+        grid_info.trail.append([grid_info.pos, grid_info.color])
+
+
 effects = {
     "blue shift - twinkle": {
         "beats": make_twinkle_beats(white),
@@ -93,7 +139,7 @@ effects = {
         "delay_lights": 0.4043245762711864,
         "skip_song": 0.0,
         "beats": [
-            grid_f(1, function=trail_ball, length=64, speed=.25, clear=False),
+            grid_f(1, function=trail_ball_fade, length=64, speed=1, clear=False),
             # b(name="blue shift - twinkle", length=8),
             # b(name="blue shift - twinkle blue", length=8),
         ],

28b5b98bda4ac481fad994cb17bccbda092d40a4
Andrew Duerig <duerig.andrew@gmail.com>
2023-10-31 15:40:14 -0400

    ok

--- winamp/load_slow_presets.py
+++ winamp/load_slow_presets.py
@@ -30,9 +30,9 @@ num_times = 300
 start_time = time.time()
 for index in tqdm(range(num_times)):
     t1 = time.time()
+    winamp_wrapper.load_preset(easy_preset_name, quiet=True)
+    winamp_wrapper.compute_frame()
     for preset_name in slow_presets:
-        winamp_wrapper.load_preset(easy_preset_name, quiet=True)
-        winamp_wrapper.compute_frame()
         winamp_wrapper.load_preset(preset_name, quiet=True)
         winamp_wrapper.compute_frame()
     runs[index] = time.time() - t1

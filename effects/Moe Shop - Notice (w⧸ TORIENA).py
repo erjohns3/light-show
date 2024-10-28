d89384dfbdf952cebd6312815ac2c305e981999e
Your Name <you@example.com>
2024-10-28 17:38:04 -0400

    Add Notice

---
+++ effects/Moe Shop - Notice (w⧸ TORIENA).py
@@ -0,0 +1,11 @@
+from effects.compiler import *
+
+effects = {
+    "Moe Shop - Notice (w\u29f8 TORIENA)": {
+        "bpm": 125,
+        "song_path": "songs/Moe Shop - Notice (w\u29f8 TORIENA).ogg",
+        "delay_lights": 0.0246,
+        "skip_song": 0.0,
+        "beats": []
+    }
+}

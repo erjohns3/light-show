4b1cc6137e7ff479d941d015a249a186617c1c30
Your Name <you@example.com>
2025-07-04 17:04:44 -0400

    hi

--- effects/Justice - Genesis.py
+++ effects/Justice - Genesis.py
@@ -174,31 +174,31 @@ effects = {
             # dooooooooooo
             b(2.75, name='Green bottom', length=7.25, intensity=(1, 0)), 
             # do-do-do-do-do..... equally spaced
-            b(10, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(11, name='Green bottom', length=1,intensity=(1, 0)), 
-            b(12, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(13, name='Green bottom', length=1, intensity=(1, 0)), 
-            b(14, name='Pink bottom', length=1,  intensity=(1, 0)), 
+            b(10, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(11, name='Green bottom', length=1,intensity=(1, 1)), 
+            b(12, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(13, name='Green bottom', length=1, intensity=(1, 1)), 
+            b(14, name='Pink bottom', length=1,  intensity=(1, 1)), 
             b(15, name='Green bottom', length=3.75, intensity=(1, 0)), 
             # do.... 
             b(18.75, name='Blue bottom', length=7.25, intensity=(1, 0)), 
 
             # repeat do-do-do-do-do equally spaced
-            b(26, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(27, name='Green bottom', length=1,intensity=(1, 0)), 
-            b(28, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(29, name='Green bottom', length=1, intensity=(1, 0)), 
-            b(30, name='Pink bottom', length=1,  intensity=(1, 0)), 
+            b(26, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(27, name='Green bottom', length=1,intensity=(1, 1)), 
+            b(28, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(29, name='Green bottom', length=1, intensity=(1, 1)), 
+            b(30, name='Pink bottom', length=1,  intensity=(1, 1)), 
             b(31, name='Green bottom', length=3.75, intensity=(1, 0)), 
             # do.... 
             b(34.75, name='Blue bottom', length=7.25, intensity=(1, 0)), 
 
             # repeat do-do-do-do-do equally spaced
-            b(42, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(43, name='Green bottom', length=1,intensity=(1, 0)), 
-            b(44, name='Pink bottom', length=1, intensity=(1, 0)), 
-            b(45, name='Green bottom', length=1, intensity=(1, 0)), 
-            b(46, name='Pink bottom', length=1,  intensity=(1, 0)), 
+            b(42, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(43, name='Green bottom', length=1,intensity=(1, 1)), 
+            b(44, name='Pink bottom', length=1, intensity=(1, 1)), 
+            b(45, name='Green bottom', length=1, intensity=(1, 1)), 
+            b(46, name='Pink bottom', length=1,  intensity=(1, 1)), 
             b(47, name='Green bottom', length=3.75, intensity=(1, 0)), 
             
             # this one is special he gets extra dooos in threes
@@ -207,8 +207,8 @@ effects = {
             b(53.25, name='Blue bottom', length=5.5, intensity=(1, 0)), 
 
             # Second set of 3
-            b(58.75, name='Yellow bottom', length=1, intensity=(1, 0)), 
-            b(59.75, name='Blue bottom', length=1.5, intensity=(1, 0)), 
+            b(58.75, name='Yellow bottom', length=1, intensity=(1, 1)), 
+            b(59.75, name='Blue bottom', length=1.5, intensity=(1, 1)), 
             b(61.25, name='Yellow bottom', length=5, intensity=(1, 0)), 
 
             # second to last
@@ -282,6 +282,12 @@ effects = {
             grid_f(321, filename='genesis_cross.jpeg', rotate_90=True, length=0.5), # color=.04 is multiplier
             grid_f(322, filename='genesis_cross.jpeg', rotate_90=True, length=1.5), # color=.04 is multiplier
             b(326.2, name='genesis wub', length=2 ),
+
+            grid_f(333, filename='genesis_cross.jpeg', rotate_90=True, length=0.5), # color=.04 is multiplier
+            grid_f(334, filename='genesis_cross.jpeg', rotate_90=True, length=1.5), # color=.04 is multiplier
+            grid_f(337, filename='genesis_cross.jpeg', rotate_90=True, length=0.5), # color=.04 is multiplier
+            grid_f(338, filename='genesis_cross.jpeg', rotate_90=True, length=1.5), # color=.04 is multiplier
+            b(332.2, name='genesis wub', length=2 ),
 
         ]
     }
